43f985c5d22380c8a5a0edc001ea5a6baebe4113
Michael Rosario <michael@elementoinc.com>
2018-08-24 20:11:57 -0400

    Update Portfolio

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,31 @@
 $(document).foundation();
 
 var typed = new Typed('.message', {
-  strings: ["building cool websites^1200", "solving web problems^1200","learning new things^1200","collaborating with teams^1200"],
+  strings: ["building cool websites.^1200", "solving web problems.^1200","learning new things.^1200","collaborating with teams.^1200"],
   typeSpeed: 40,
   backSpeed: 0,
   cursorChar: '|',
   loop: true,
 });
+
+
+$(window).on("scroll",function() {
+  
+  if($("#intro").offset().top == 0){
+    $(".learnMain").show();
+  } else {
+    $(".learnMain").hide();
+  }
+
+});
+
+$(".scrollTo").on("click",function(e){
+	e.preventDefault();
+	var target = $(this).attr('data-target');
+	var pos = $('#'+target).offset().top;
+  	$('html, body').animate({ scrollTop: pos }, 200);
+
+  	return false;
+});
+
 
